d3a441e0ea45bde2f9e3ad6341fd5016c27d26af
Charlotte Pierce <charlotte@malformed-bits.com>
2014-01-14 17:12:24 +1100

    Added example commands for edge and node rewriting plants.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- src/main.py
+++ src/main.py
@@ -12,39 +12,44 @@ if __name__ == '__main__':
 	turtle.pensize(1)
 	turtle.hideturtle() # speeds up drawing significantly
 	turtle.speed(0) # disable animation
-	start_pos = turtle.pos()
-	start_heading = turtle.heading()
 
-	cmd = CommandString('F', {'F' : 'F[+F]F[-F]F'}, 5, 25.7)
+	# Edge rewriting
+# 	cmd = CommandString('F', {'F' : 'F[+F]F[-F]F'}, 5, 25.7)
+# 	cmd = CommandString('F', {'F' : 'F[+F]F[-F][F]'}, 5, 20)
+# 	cmd = CommandString('F', {'F' : 'FF-[-F+F+]+[+F-F-F]'}, 5, 22.5)
+
+	# Node rewriting
+# 	cmd = CommandString('X', {'X' : 'F[+X]F[-X]+X', 'F' : 'FF'}, 5, 20)
+# 	cmd = CommandString('X', {'X' : 'F[+X][-X]FX', 'F' : 'FF'}, 5, 25.7)
+	cmd = CommandString('X', {'X' : 'F-[[X]+X]+F[+FX]-X', 'F' : 'FF'}, 5, 22.5)
 
 	state_stack = []
-	for x in range(7):
-		print 'Command:', cmd.command_string, '(' + str(cmd.step_size) + ')'
-		for c in cmd.command_string:
-			if c == 'F':
-				turtle.pendown()
-				turtle.forward(cmd.step_size)
-			elif c == 'f':
-				turtle.penup()
-				turtle.forward(cmd.step_size)
-			elif c == '+':
-				turtle.right(cmd.angle_increment)
-			elif c == '-':
-				turtle.left(cmd.angle_increment)
-			elif c == '[':
-				state_stack.append((turtle.pos(), turtle.heading()))
-			elif c == ']':
-				turtle.penup()
-				pos, heading = state_stack.pop()
-				turtle.setpos(pos)
-				turtle.setheading(heading)
+	for x in range(5):
+		print 'Evolving command string', str(x)
 
 		cmd.evolve()
-		turtle.penup()
-		turtle.setpos(start_pos)
-		turtle.setheading(start_heading)
 
-	print 'Done'
+	print 'Drawing...',
+	for c in cmd.command_string:
+		if c == 'F':
+			turtle.pendown()
+			turtle.forward(cmd.step_size)
+		elif c == 'f':
+			turtle.penup()
+			turtle.forward(cmd.step_size)
+		elif c == '+':
+			turtle.right(cmd.angle_increment)
+		elif c == '-':
+			turtle.left(cmd.angle_increment)
+		elif c == '[':
+			state_stack.append((turtle.pos(), turtle.heading()))
+		elif c == ']':
+			turtle.penup()
+			pos, heading = state_stack.pop()
+			turtle.setpos(pos)
+			turtle.setheading(heading)
+
+	print 'done'
 
 	win.exitonclick()
 
